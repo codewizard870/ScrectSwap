6266e00a7b669b34d0259bfaaf2e1d0074fd588e
Assaf Morami <assaf.morami@gmail.com>
2021-03-31 10:49:54 +0300

    swap: remove beta warning on mainnet

--- src/components/Swap/BetaWarning.tsx
+++ src/components/Swap/BetaWarning.tsx
@@ -3,6 +3,10 @@ import { SigningCosmWasmClient } from 'secretjs';
 import { Message } from 'semantic-ui-react';
 
 export const BetaWarning = ({ secretjs }: { secretjs: SigningCosmWasmClient }) => {
+  if (process.env.ENV !== 'TESTNET') {
+    return null;
+  }
+
   return (
     <Message warning>
       <Message.Header>Feedback channels:</Message.Header>

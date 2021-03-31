de4d9a52d416c00e45edf1ecdfdbc106bf952b25
Sandy Corsillo <the_dusky@hey.com>
2021-03-31 16:39:58 -0400

    Adding fonts and select token button changed

--- src/pages/Swap/TokenSelector/TokenSelectorButton.tsx
+++ src/pages/Swap/TokenSelector/TokenSelectorButton.tsx
@@ -3,13 +3,15 @@ import * as styles from './styles.styl';
 import cn from 'classnames';
 import { TokenButton } from './TokenButton';
 import { SwapToken } from '../types/SwapToken';
+import {ExpandIcon} from '../../../ui/Icons/ExpandIcon';
 
 export const TokenSelectorButton = (props: { token?: SwapToken; onClick?: any }) => {
   const isEmpty = !props?.token;
 
   return isEmpty ? (
     <button className={cn(styles.selectATokenButton)} onClick={props.onClick}>
-      Select a token
+      Select a token &nbsp;
+      <ExpandIcon />
     </button>
   ) : (
     <TokenButton token={props.token} onClick={props.onClick} />

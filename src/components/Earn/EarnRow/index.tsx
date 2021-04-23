7bd4a7a6e27a8e55bc570d67a5930a6471db7fc3
Clipart <lourencobrutocosta@gmail.com>
2021-04-23 15:01:48 +0100

    Sort Earn List

--- src/components/Earn/EarnRow/index.tsx
+++ src/components/Earn/EarnRow/index.tsx
@@ -14,6 +14,32 @@ import { divDecimals, formatWithTwoDecimals, zeroDecimalsFormatter } from '../..
 import { Text } from '../../Base';
 import ScrtTokenBalance from '../ScrtTokenBalance';
 
+
+export const calculateAPY = (token: RewardsToken, price: number, priceUnderlying: number) => {
+  // console.log(Math.round(Date.now() / 1000000))
+  // deadline - current time, 6 seconds per block
+  const timeRemaining = (token.deadline - 3004867) * 6.22 + 1618321783 - Math.round(Date.now() / 1000);
+
+  // (token.deadline - Math.round(Date.now() / 1000000) );
+  const pending = Number(divDecimals(token.remainingLockedRewards, token.rewardsDecimals)) * price;
+
+  // this is already normalized
+  const locked = Number(token.totalLockedRewards);
+
+  //console.log(`pending - ${pending}; locked: ${locked}, time remaining: ${timeRemaining}`)
+  return (((pending * 100) / locked) * (3.154e7 / timeRemaining)).toFixed(0);
+};
+
+export const apyString = (token: RewardsToken) => {
+  const apy = Number(calculateAPY(token, Number(token.rewardsPrice), Number(token.price)));
+  if (isNaN(apy) || 0 > apy) {
+    return `0%`;
+  }
+
+  const apyStr = zeroDecimalsFormatter.format(Number(apy));
+
+  return `${apyStr}%`;
+};
 interface RewardsToken {
   name: string;
   decimals: string;
@@ -36,48 +62,21 @@ interface RewardsToken {
   deadline: number;
   rewardsSymbol?: string;
 }
-
-const calculateAPY = (token: RewardsToken, price: number, priceUnderlying: number) => {
-  // console.log(Math.round(Date.now() / 1000000))
-  // deadline - current time, 6 seconds per block
-  const timeRemaining = (token.deadline - 3004867) * 6.22 + 1618321783 - Math.round(Date.now() / 1000);
-
-  // (token.deadline - Math.round(Date.now() / 1000000) );
-  const pending = Number(divDecimals(token.remainingLockedRewards, token.rewardsDecimals)) * price;
-
-  // this is already normalized
-  const locked = Number(token.totalLockedRewards);
-
-  //console.log(`pending - ${pending}; locked: ${locked}, time remaining: ${timeRemaining}`)
-  return (((pending * 100) / locked) * (3.154e7 / timeRemaining)).toFixed(0);
-};
-
-const apyString = (token: RewardsToken) => {
-  const apy = Number(calculateAPY(token, Number(token.rewardsPrice), Number(token.price)));
-  if (isNaN(apy) || 0 > apy) {
-    return `0%`;
-  }
-
-  const apyStr = zeroDecimalsFormatter.format(Number(apy));
-
-  return `${apyStr}%`;
-};
-
 @observer
 class EarnRow extends Component<
-  {
-    userStore: UserStoreEx;
-    token: RewardsToken;
-    notify: Function;
-    callToAction: string;
-  },
-  {
-    activeIndex: Number;
-    depositValue: string;
-    withdrawValue: string;
-    claimButtonPulse: boolean;
-    pulseInterval: number;
-  }
+{
+  userStore: UserStoreEx;
+  token: RewardsToken;
+  notify: Function;
+  callToAction: string;
+},
+{
+  activeIndex: Number;
+  depositValue: string;
+  withdrawValue: string;
+  claimButtonPulse: boolean;
+  pulseInterval: number;
+}
 > {
   state = {
     activeIndex: -1,
@@ -182,8 +181,7 @@ class EarnRow extends Component<
                 if (value) {
                   this.props.notify(
                     'success',
-                    `Created a viewing key for ${this.props.token.display_props.symbol !== 'SEFI' ? 's' : ''}${
-                      this.props.token.display_props.symbol
+                    `Created a viewing key for ${this.props.token.display_props.symbol !== 'SEFI' ? 's' : ''}${this.props.token.display_props.symbol
                     }`,
                   );
                 } else {
@@ -210,8 +208,7 @@ class EarnRow extends Component<
                 if (value) {
                   this.props.notify(
                     'success',
-                    `Created a viewing key for ${this.props.token.display_props.symbol !== 'SEFI' ? 's' : ''}${
-                      this.props.token.display_props.symbol
+                    `Created a viewing key for ${this.props.token.display_props.symbol !== 'SEFI' ? 's' : ''}${this.props.token.display_props.symbol
                     } rewards`,
                   );
                 } else {

fca62d70701da521e91f7ea21d7196a15fa09d1a
Itzik Grossman <itzik@keytango.io>
2021-05-01 18:33:00 +0300

    Fixed weird balance loading thing when switching tokens

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -506,7 +506,7 @@ export class SwapRouter extends React.Component<
       selectedPairRoutes: routes,
     });
 
-    this.refreshBalances({ tokens: [token0, token1], pair: selectedPair });
+    //this.refreshBalances({ tokens: [token0, token1], pair: selectedPair });
   };
 
   updatePairs = async () => {

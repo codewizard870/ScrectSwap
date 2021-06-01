af048ee05958553a63a78b31e07016d8bd22475d
David Rodriguez <david@stakeordie.com>
2021-06-01 16:29:01 -0700

    Cashback re-design

--- src/pages/Cashback/index.tsx
+++ src/pages/Cashback/index.tsx
@@ -89,8 +89,8 @@ export const Cashback =observer((props)=>{
 
     const topRightChart = 514;
     const topLeftChart = 14;
-    const topLeftLabel = 50.5;
-    const topRightLabel = 575.5;
+    const topLeftLabel = 60.5;
+    const topRightLabel = 560.5;
     const maxLimit= getMaxValue();
 
     const sefi_earned = localStorage.getItem('total_sefi_earned')
@@ -112,15 +112,14 @@ export const Cashback =observer((props)=>{
     }else if(rateCSHBK <= minLimit){
       //Equal or LOWER than minimun limit
       xPositionChart =topLeftChart
-      xPositionLabel =topLeftLabel
-      xPositionArrow=topLeftLabel-5;
-      minLimit=0;
+      xPositionArrow=topLeftLabel;
+      xPositionLabel =topLeftLabel+5
 
     }else{
       //Calculations
       const b=125
       const a= b/minLimit
-      const bLabel=131.25
+      const bLabel=125
       const aLabel=bLabel/minLimit
       xPositionChart = parseFloat((a * rateCSHBK - b).toFixed(2))+topLeftChart;
       xPositionLabel = parseFloat((aLabel * rateCSHBK - bLabel).toFixed(2))+topLeftLabel;
@@ -143,89 +142,91 @@ export const Cashback =observer((props)=>{
         <PageContainer>
           <Box className="cashback-container">
             <h1 className={`trade-more__title ${theme.currentTheme}`}>Trade more. Earn More.</h1>
-            <div className={`cashback-container__card ${theme.currentTheme}`}>
-              <div className="congratulations-container">
-                {
-                  (user.isUnconnected == 'true')
-                  ? <>
-                      <h1 className='connect_wallet'>PLEASE CONNECT YOUR WALLET</h1>
-                      <p>You haven't traded recently on </p>
-                      <h3><strong>secret</strong>swap</h3>
-                      <p>You currently have</p>
-                      <h2>0 CSHBK </h2>
-                    </> 
-                  : (hasCashback)
+            <div className={`box-shadow ${theme.currentTheme}`}>
+              <div className={`cashback-container__card ${theme.currentTheme}`}>
+                <div className="congratulations-container">
+                  {
+                    (user.isUnconnected == 'true')
                     ? <>
-                        <h1>Congratulations</h1>
-                        <p>For trading on</p>
+                        <h1 className='connect_wallet'>PLEASE CONNECT YOUR WALLET</h1>
+                        <p>You haven't traded recently on </p>
                         <h3><strong>secret</strong>swap</h3>
-                        <p>You have earned</p>
-                        <h2>
-                            {(user?.balanceCSHBK == 'Unlock')
-                              ?<p><strong >{unlockJsx({onClick:createCSHBKViewingKey})}</strong></p>
-                              :balanceCSHBK 
-                            } 
-                          	&nbsp;CSHBK 
-                        </h2>
-                        <p>that you can trade for</p>
-                        <h2>{user.expectedSEFIFromCSHBK} SEFI</h2>
+                        <p>You currently have</p>
+                        <h2>0 CSHBK </h2>
+                      </> 
+                    : (hasCashback)
+                      ? <>
+                          <h1>Congratulations</h1>
+                          <p>For trading on</p>
+                          <h3><strong>secret</strong>swap</h3>
+                          <p>You have earned</p>
+                          <h2>
+                              {(user?.balanceCSHBK == 'Unlock')
+                                ?<p><strong >{unlockJsx({onClick:createCSHBKViewingKey})}</strong></p>
+                                :balanceCSHBK 
+                              } 
+                              &nbsp;CSHBK 
+                          </h2>
+                          <p>that you can trade for</p>
+                          <h2>{user.expectedSEFIFromCSHBK} SEFI</h2>
+                        </>
+                      : <>
+                        <h1>Keep Trading!</h1> 
+                        <p>You haven't traded recently on </p>
+                        <h3><strong>secret</strong>swap</h3>
+                        <p>You currently have</p>
+                        <h2>0 CSHBK </h2>
                       </>
-                    : <>
-                      <h1>Keep Trading!</h1> 
-                      <p>You haven't traded recently on </p>
-                      <h3><strong>secret</strong>swap</h3>
-                      <p>You currently have</p>
-                      <h2>0 CSHBK </h2>
-                    </>
-                }
-                
+                  }
+                  
+                </div>
+                <div className="call-toAction__container">
+                  <img  src=
+                      {
+                        (isNaN(balanceCSHBK) || balanceCSHBK == 0)
+                          ?"/static/cashback_logo_gray.png"
+                          :"/static/cashback_logo.png"
+                      } alt="Cashback logo"  />
+                  <Button 
+                    loading={loading}
+                    disabled={!hasCashback || user?.balanceCSHBK == 'Unlock'}
+                    className="redeem-sefi__button" 
+                    onClick={burnSEFI}>
+                      Redeem for SEFI 
+                  </Button>
+                </div>
               </div>
-              <div className="call-toAction__container">
-                <img  src=
-                    {
-                      (isNaN(balanceCSHBK) || balanceCSHBK == 0)
-                        ?"/static/cashback_logo_gray.png"
-                        :"/static/cashback_logo.png"
-                    } alt="Cashback logo"  />
-                <Button 
-                  loading={loading}
-                  disabled={!hasCashback || user?.balanceCSHBK == 'Unlock'}
-                  className="redeem-sefi__button" 
-                  onClick={burnSEFI}>
-                    Redeem for SEFI 
-                </Button>
-              </div>
-            </div>
-            <div className={`rate-bar__container ${theme.currentTheme}`}>
-              <div className='rate-labels__container'>
-                  <svg width="631" height="94" viewBox="0 0 631 94" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <text x={xPositionLabel} textAnchor='middle' y='30%' fill={fontColor}>Current Rate</text>
-                      <text x={xPositionLabel} textAnchor='middle' y='60%' fill={ratioColor}>{rateCSHBK}</text>
-                      <ArrowDown x={xPositionArrow} y='25' stroke={fontColor} width='10px'/>
+              <div className={`rate-bar__container ${theme.currentTheme}`}>
+                <div className='rate-labels__container'>
+                    <svg width="631" height="94" viewBox="0 0 631 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <text x={xPositionLabel} textAnchor='middle' y='30%' fill={fontColor}>Current Rate</text>
+                        <text x={xPositionLabel} textAnchor='middle' y='60%' fill={ratioColor}>{rateCSHBK}</text>
+                        <ArrowDown x={xPositionArrow} y='25' stroke={fontColor} width='10px'/>
+                    </svg>
+                </div>
+                <div className="rate-svg__container">
+                  <svg width="528" height="94" viewBox="0 0 528 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <text fill="#FF726E" x={topLeftChart} y='10' textAnchor='middle'>Bad</text>
+                    <text fill="#79CC81" x={topRightChart - 5 } y='10' textAnchor='middle'>Good</text>
+                    <rect x="4" y="21" width="520" height="22" rx="11" fill="url(#paint0_linear)"/>
+                    <path d={`M${xPositionChart} 21V43`} stroke={fontColor}/>
+                    <path d="M14 53V73" stroke={fontColor}/>
+                    <path d="M139 53V63" stroke={fontColor}/>
+                    <path d="M264 53V73" stroke={fontColor}/>
+                    <path d="M389 53V63" stroke={fontColor}/>
+                    <path d="M514 53V73" stroke={fontColor}/>
+                    <text fill={fontColor} x={topLeftChart} y='90' textAnchor='middle'>{minLimit}</text>
+                    <text fill={fontColor} x={topRightChart } y='90' textAnchor='middle'>{maxLimit}</text>
+                    <text fill={fontColor} x={(topRightChart-topLeftChart)/2 + topLeftChart} y='90' textAnchor='middle'>{midValue}</text>
+                    <defs>
+                    <linearGradient id="paint0_linear" x1="14" y1="31.9999" x2="514" y2="32.0001" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#FF726E"/>
+                    <stop offset="0.507078" stopColor="#F8EC99"/>
+                    <stop offset="1" stopColor="#79CC81"/>
+                    </linearGradient>
+                    </defs>
                   </svg>
-              </div>
-              <div className="rate-svg__container">
-                <svg width="528" height="94" viewBox="0 0 528 94" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <text fill="#FF726E" x={topLeftChart} y='10' textAnchor='middle'>Bad</text>
-                  <text fill="#79CC81" x={topRightChart - 5 } y='10' textAnchor='middle'>Good</text>
-                  <rect x="4" y="21" width="520" height="22" rx="11" fill="url(#paint0_linear)"/>
-                  <path d={`M${xPositionChart} 21V43`} stroke={fontColor}/>
-                  <path d="M14 53V73" stroke={fontColor}/>
-                  <path d="M139 53V63" stroke={fontColor}/>
-                  <path d="M264 53V73" stroke={fontColor}/>
-                  <path d="M389 53V63" stroke={fontColor}/>
-                  <path d="M514 53V73" stroke={fontColor}/>
-                  <text fill={fontColor} x={topLeftChart} y='90' textAnchor='middle'>{minLimit}</text>
-                  <text fill={fontColor} x={topRightChart } y='90' textAnchor='middle'>{maxLimit}</text>
-                  <text fill={fontColor} x={(topRightChart-topLeftChart)/2 + topLeftChart} y='90' textAnchor='middle'>{midValue}</text>
-                  <defs>
-                  <linearGradient id="paint0_linear" x1="14" y1="31.9999" x2="514" y2="32.0001" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#FF726E"/>
-                  <stop offset="0.507078" stopColor="#F8EC99"/>
-                  <stop offset="1" stopColor="#79CC81"/>
-                  </linearGradient>
-                  </defs>
-                </svg>
+                </div>
               </div>
             </div>
             <div className={`additional-info__container ${theme.currentTheme}`}>

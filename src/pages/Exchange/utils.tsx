e0196a2600ac133a54ac74074612c80f6d734eed
Itzik Grossman <itzik@keytango.io>
2021-06-08 20:14:10 +0300

    Staked.us -> Staked

--- src/pages/Exchange/utils.tsx
+++ src/pages/Exchange/utils.tsx
@@ -102,57 +102,55 @@ export const WrongNetwork = (props: { networkSelected: NETWORKS }) => {
   );
 };
 
-
 export enum Signer {
   figment,
   bharvest,
   citadel,
   enigma,
-  staked
+  staked,
 }
 
 export const signerToString = (signer: Signer): string => {
-    switch (signer) {
-      case Signer.bharvest:
-        return "B-Harvest"
-      case Signer.citadel:
-        return "Citadel.One"
-      case Signer.enigma:
-        return "Enigma";
-      case Signer.figment:
-        return "Figment";
-      case Signer.staked:
-        return "Staked.us"
-    }
-}
+  switch (signer) {
+    case Signer.bharvest:
+      return 'B-Harvest';
+    case Signer.citadel:
+      return 'Citadel.One';
+    case Signer.enigma:
+      return 'Enigma';
+    case Signer.figment:
+      return 'Figment';
+    case Signer.staked:
+      return 'Staked';
+  }
+};
 
 export const signerAddresses = {
   [NETWORKS.ETH]: {
-    "0xd76c427fc6e48fc94d1c62e0a23c4bf07a22a3fb": Signer.bharvest,
-    "0xcc3040b283ff0df84073a5a446d88d10ea329460": Signer.citadel,
-    "0x9d06d59677b412c48f5f8546b45b9ea694a99698": Signer.enigma,
-    "0x42194527ddccaee189313d77458ae491fa41256a": Signer.staked,
-    "0x189fbd54bd7194cd4e49cbc067e92cfd6dc8281d": Signer.figment,
+    '0xd76c427fc6e48fc94d1c62e0a23c4bf07a22a3fb': Signer.bharvest,
+    '0xcc3040b283ff0df84073a5a446d88d10ea329460': Signer.citadel,
+    '0x9d06d59677b412c48f5f8546b45b9ea694a99698': Signer.enigma,
+    '0x42194527ddccaee189313d77458ae491fa41256a': Signer.staked,
+    '0x189fbd54bd7194cd4e49cbc067e92cfd6dc8281d': Signer.figment,
   },
   [NETWORKS.BSC]: {
-    "0xd76c427fc6e48fc94d1c62e0a23c4bf07a22a3fb": Signer.bharvest,
-    "0xcc3040b283ff0df84073a5a446d88d10ea329460": Signer.citadel,
-    "0x08e54c84d61e9db2ed7ea53e2216276d75b5b426": Signer.enigma,
-    "0x42194527ddccaee189313d77458ae491fa41256a": Signer.staked,
-    "0x189fbd54bd7194cd4e49cbc067e92cfd6dc8281d": Signer.figment,
-  }
-}
+    '0xd76c427fc6e48fc94d1c62e0a23c4bf07a22a3fb': Signer.bharvest,
+    '0xcc3040b283ff0df84073a5a446d88d10ea329460': Signer.citadel,
+    '0x08e54c84d61e9db2ed7ea53e2216276d75b5b426': Signer.enigma,
+    '0x42194527ddccaee189313d77458ae491fa41256a': Signer.staked,
+    '0x189fbd54bd7194cd4e49cbc067e92cfd6dc8281d': Signer.figment,
+  },
+};
 
 export type HealthStatus = {
-  time: Date,
-  status: boolean
-}
+  time: Date;
+  status: boolean;
+};
 
 export type HealthStatusDetailed = Record<Signer, HealthStatus>;
 export const SignerTypes = [Signer.figment, Signer.enigma, Signer.citadel, Signer.bharvest, Signer.staked];
 
-export function healthFromStatus (health: HealthStatusDetailed): boolean {
-
+export function healthFromStatus(health: HealthStatusDetailed): boolean {
   if (!health) {
     return false;
   }
@@ -167,7 +165,7 @@ export function healthFromStatus (health: HealthStatusDetailed): boolean {
       }
     }
   }
-  return online >= Number(process.env.SIG_THRESHOLD) && leaderOnline
+  return online >= Number(process.env.SIG_THRESHOLD) && leaderOnline;
 }
 
 export type NetworkTemplateInterface = {
@@ -182,71 +180,68 @@ export type NetworkTemplateInterface = {
 };
 
 export const NetworkTemplate = (props: { template: NetworkTemplateInterface; user: any }) => {
-
   let overAllHealth = healthFromStatus(props.template.health);
 
   return (
-  <Box direction="column">
-    <Box direction="row" align={'start'} margin={{ top: 'xxsmall' }}>
-      <Box direction="column" style={{ marginRight: 7, minWidth: 40 }} align="center">
-        <img style={{ marginBottom: 5 }} height="37" src={props.template.networkImage} alt="network image" />
-      </Box>
-
-      <Box direction="column">
-        <Title bold color={'#30303D'} margin={{ bottom: 'xxsmall' }}>
-          {props.template.name}
-        </Title>
-        <Text size="medium" bold color={'#748695'}>
-          {props.template.wallet}
-        </Text>
-
-      </Box>
-    </Box>
-    <HealthPopup health={props.template.health}>
-      <Box direction="row"  align={'start'} margin={{ top: 'xxsmall' }}>
+    <Box direction="column">
+      <Box direction="row" align={'start'} margin={{ top: 'xxsmall' }}>
         <Box direction="column" style={{ marginRight: 7, minWidth: 40 }} align="center">
-          <IconUI style={{ margin: 0 }} className={'circle'} color={overAllHealth ? 'green' : 'red'} />
+          <img style={{ marginBottom: 5 }} height="37" src={props.template.networkImage} alt="network image" />
         </Box>
-        <Box direction="column">
-        <Text size="xsmall" color={'#748695'}>
-          {overAllHealth ? 'Live' : 'Down'}
-        </Text>
-        </Box>
-      </Box>
-    </HealthPopup>
-    {props.template.symbol && (
-      <Box
-        pad="xsmall"
-        direction="row"
-        align={'center'}
-        margin={{ top: 'xxsmall' }}
-        className={styles.networktemplatetoken}
-      >
-        {props.template.image && (
-          <img src={props.template.image} style={{ width: 20, margin: '0 5' }} alt={props.template.symbol} />
-        )}
-        {props.template.amount === 'loading' ? (
-          <Loader type="ThreeDots" color="#00BFFF" height="1em" width="1.5em" style={{ margin: '0 10' }} />
-        ) : props.template.amount === unlockToken ? (
-          <Box direction="row" style={{ margin: '0 5' }}>
-            <ViewingKeyIcon user={props.user} />
-          </Box>
-        ) : (
-          <Text bold size="medium" style={{ margin: '0 5' }}>
-            <span style={{ color: props.template.amount === wrongNetwork ? '#c5bb2e' : '#30303D' }}>
-              {props.template.amount}
-            </span>
-          </Text>
-        )}
-        <Text bold style={{ margin: '0 5' }} color="#748695" size="medium">
-          {props.template.symbol}
-        </Text>
-      </Box>
-    )}
-  </Box>
-);
-}
 
+        <Box direction="column">
+          <Title bold color={'#30303D'} margin={{ bottom: 'xxsmall' }}>
+            {props.template.name}
+          </Title>
+          <Text size="medium" bold color={'#748695'}>
+            {props.template.wallet}
+          </Text>
+        </Box>
+      </Box>
+      <HealthPopup health={props.template.health}>
+        <Box direction="row" align={'start'} margin={{ top: 'xxsmall' }}>
+          <Box direction="column" style={{ marginRight: 7, minWidth: 40 }} align="center">
+            <IconUI style={{ margin: 0 }} className={'circle'} color={overAllHealth ? 'green' : 'red'} />
+          </Box>
+          <Box direction="column">
+            <Text size="xsmall" color={'#748695'}>
+              {overAllHealth ? 'Live' : 'Down'}
+            </Text>
+          </Box>
+        </Box>
+      </HealthPopup>
+      {props.template.symbol && (
+        <Box
+          pad="xsmall"
+          direction="row"
+          align={'center'}
+          margin={{ top: 'xxsmall' }}
+          className={styles.networktemplatetoken}
+        >
+          {props.template.image && (
+            <img src={props.template.image} style={{ width: 20, margin: '0 5' }} alt={props.template.symbol} />
+          )}
+          {props.template.amount === 'loading' ? (
+            <Loader type="ThreeDots" color="#00BFFF" height="1em" width="1.5em" style={{ margin: '0 10' }} />
+          ) : props.template.amount === unlockToken ? (
+            <Box direction="row" style={{ margin: '0 5' }}>
+              <ViewingKeyIcon user={props.user} />
+            </Box>
+          ) : (
+            <Text bold size="medium" style={{ margin: '0 5' }}>
+              <span style={{ color: props.template.amount === wrongNetwork ? '#c5bb2e' : '#30303D' }}>
+                {props.template.amount}
+              </span>
+            </Text>
+          )}
+          <Text bold style={{ margin: '0 5' }} color="#748695" size="medium">
+            {props.template.symbol}
+          </Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 export const CopyRow = (props: { label: string; value: string; rawValue: string }) => (
   <HeadShake bottom>

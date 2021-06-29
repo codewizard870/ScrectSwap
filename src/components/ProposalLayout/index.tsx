3101e155d0715be2b6ae4a9b7a57e94d8d972b4d
David SoD <david@stakeordie.com>
2021-06-29 12:25:20 -0700

    Implementing proposalLayout

---
+++ src/components/ProposalLayout/index.tsx
@@ -0,0 +1,37 @@
+import { BaseContainer,PageContainer} from 'components';
+import { Box } from 'grommet';
+import React, { ReactChild } from 'react';
+import { Link } from 'react-router-dom';
+import { useStores } from 'stores';
+import  './style.scss';
+
+const ProposalLayout = (props:{
+    children:ReactChild,
+    width?:string
+})=>{
+    const {theme} = useStores();
+    return (
+        <BaseContainer>
+            <PageContainer>
+            <Box
+                className={`proposal-layout ${theme.currentTheme}`}
+                pad={{ horizontal: '136px', top: 'small' }}
+                style={{ alignItems: 'center' }}
+            >  
+                <div style={{width:props.width || '100%'}}>
+                    <Link className='go-back' to='/governance' >  
+                        <img src="/static/arrow-left.svg" alt="go-back" />
+                        <h3>Proposal</h3>
+                    </Link>
+                <div className='proposal-layout-content'> 
+                        {props.children}
+                </div>
+
+                </div>
+            </Box>
+            </PageContainer>
+        </BaseContainer>
+    )
+}
+
+export default  ProposalLayout;

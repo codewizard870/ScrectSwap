ede2ba431c81cac9f862d3a62cbca494c948d402
jsandovalg <jorge@stakeordie.com>
2021-07-28 20:27:59 -0700

    show staked in new y old pool in migrations cards

---
+++ src/ui/Spinner/SpinnerDashes.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './spinnerDashes.scss'
+
+const SpinnerDashes = () => {
+    return (
+        <>
+            <div className="spinner">
+                <div className="bounce1"></div>
+                <div className="bounce2"></div>
+                <div className="bounce3"></div>
+            </div>
+        </>
+    )
+}
+
+export default SpinnerDashes

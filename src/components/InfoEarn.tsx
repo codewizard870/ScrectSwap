8fc8d8f1cd897ce62430cfa5817fc719598fa89c
Assaf Morami <assaf.morami@gmail.com>
2021-09-19 07:29:06 +0300

    secret-3 messages

--- src/components/InfoEarn.tsx
+++ src/components/InfoEarn.tsx
@@ -12,31 +12,19 @@ export const InfoEarn = ({ title }: { title: string }) => (
     ) : null}
     <div>
       <p>
-        The higher the number of addresses and amount of funds interacting with the bridge, the higher the anonymity
-        guarantees of the bridge. <b>Let's earn yield and protect our privacy!</b>
-        <ul>
-          <li>
-            1. In order to earn rewards, users must deposit secretTokens to a rewards contract by choosing Earn.
-            <br />{' '}
-            <span>
-              Note: If you have created a secretToken, please make sure you have already created the viewing key to see
-              your balances. You can always create a viewing key by clicking on lock icon.
-            </span>
-          </li>
-          <li>
-            2. Once users deposit secretTokens to the rewards contracts, they will start to accumulate bridge rewards in
-            secretSCRT. In order to view your rewards balance and your deposited secret tokens make sure you have
-            created a viewing key for each reward pool.
-          </li>
-          <li>
-            3. When users withdraw or deposit secretTokens from/to rewards contract, the bridge rewards they have
-            accumulated will be automatically transferred to their wallet (Keplr)
-          </li>
-          <li>
-            4. Users can use Keplr app to convert secretSCRT (privacy token) to SCRT (Secret Network native coin) using
-            Keplr app to participate in staking.
-          </li>
-        </ul>
+        Due to a smart contract vulnerability that has since been fixed, earn pools are not currently generating
+        rewards. New pools will be released soon which will allow you to migrate and continue to earn rewards as normal.
+      </p>
+
+      <p>
+        Unfortunately, due to the nature of the vulnerability, unclaimed rewards are no longer available. This an
+        unavoidable consequence of the vulnerability and the need to fix it before restoring service to SecretSwap.
+      </p>
+
+      <p>
+        We are still examining ways to restore unclaimed rewards, but doing so is a non-trivial issue that should not
+        delay the relaunch of SecretSwap. Stay tuned for more updates on the new LP staking rewards, and restoration of
+        other core network services such as the Secret Bridge
       </p>
     </div>
   </Box>

d3936d6314f1b4a1b875acb0de7141691c0b0b72
7Two1 <users.noreply.github.com>
2021-12-01 18:03:41 +0000

    Change raise Gas for SEFI staking deposit/withdraw.

--- src/utils/gasPrices.ts
+++ src/utils/gasPrices.ts
@@ -1,11 +1,11 @@
 export const GAS_FOR_CLAIM = 135_000;
 export const PROPOSAL_BASE_FEE = 10_000;
-export const GAS_FOR_EARN_DEPOSIT = 135_000;
-export const GAS_FOR_EARN_WITHDRAW = 135_000;
+export const GAS_FOR_EARN_DEPOSIT = 200_000;
+export const GAS_FOR_EARN_WITHDRAW = 200_000;
 export const GAS_FOR_CREATE_PAIR = 350_000;
 export const GAS_FOR_PROVIDE = 125_000;
 export const GAS_FOR_APPROVE = 75_000;
-export const GAS_FOR_WITHDRAW_LP_FROM_SWAP = 125_000;
+export const GAS_FOR_WITHDRAW_LP_FROM_SWAP = 190_000;
 export const GAS_FOR_BASE_SWAP_ROUTE = 135_000;
 export const GAS_FOR_SWAP_NATIVE_COIN = 135_000;
 export const GAS_FOR_SWAP_DIRECT = 135_000;
